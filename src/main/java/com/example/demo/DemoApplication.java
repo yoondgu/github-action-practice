e38f14ce6b26ee1686a43964de28a45fe5601302
yoondgu <doyou2ing@naver.com>
2023-07-25 00:42:26 +0900

    feat: push test for action build check

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
