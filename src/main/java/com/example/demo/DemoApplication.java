b56d3e9663e25bec8f8c41079084dfd45e62bb16
yoondgu <doyou2ing@naver.com>
2023-07-25 00:43:35 +0900

    feat: pull request test for action build check

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test2");
+		System.out.println("pull request");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
