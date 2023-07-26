e32123e6782f5b0ef8b32306a1d67d8c8775b486
yoondgu <doyou2ing@naver.com>
2023-07-26 16:56:23 +0900

    feat: merge test for deploy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("pull request");
+		System.out.println("merge");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
