3da29328a824e4cd09e8a23da56e6c7f11ca877f
Doy <doyou2ing@naver.com>
2023-07-26 21:14:12 +0900

    Revert "Revert "Revert "push 이벤트로 job 실행되는지 확인" (#17)" (#18)" (#19)
    This reverts commit d3d9c39d90ecacfbad63cdfe66e0c4f9ff6bd92b.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("merge 11");
+		System.out.println("merge 10");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
