e11805f46fc33ceffc33908f2e77536d6c3f6403
yoondgu <doyou2ing@naver.com>
2023-07-26 17:28:49 +0900

    feat: PR merge됐을 때만 build하는지 확인

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("merge 5");
+		System.out.println("merge 6");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
